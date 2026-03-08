ee8ac9b439fd751089bd92318778b85776291256
Depatin Florian <depatin@gmail.com>
2026-03-08 21:44:04 +0100

    feat(ci): test with broken style import

--- assets/app.ts
+++ assets/app.ts
@@ -6,9 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-
-const broken = ;
+import './styles/app2.scss';
 
 console.log('TypeScript chargé');
 
